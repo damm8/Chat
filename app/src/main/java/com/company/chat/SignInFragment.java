ea2e2e73474c093847ab38edc4b47378c5c07d14
gerardfp <gerardfp@gmail.com>
2021-02-01 18:59:58 +0100

    --

--- app/src/main/java/com/company/chat/SignInFragment.java
+++ app/src/main/java/com/company/chat/SignInFragment.java
@@ -5,17 +5,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.company.chat.databinding.FragmentSignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignInFragment extends Fragment {
 
     private FragmentSignInBinding binding;
+    private FirebaseAuth mAuth;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,5 +37,25 @@ public class SignInFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        navController = Navigation.findNavController(view);
+
+        binding.emailSignIn.setOnClickListener(v -> {
+            String email = binding.email.getText().toString();
+            String password = binding.password.getText().toString();
+
+            // registrar
+
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    navController.navigate(R.id.action_signInFragment_to_chatFragment);
+                } else {
+                    Toast.makeText(requireContext(), "Error sign in", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // loguear
+        });
     }
 }
