bd004723a6f983124b9f7b7ed014b28647dafb6a
gerardfp <gerardfp@gmail.com>
2021-02-01 19:31:36 +0100

    --

--- app/src/main/java/com/company/chat/SignInFragment.java
+++ app/src/main/java/com/company/chat/SignInFragment.java
@@ -8,28 +8,24 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.company.chat.databinding.FragmentSignInBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 
 public class SignInFragment extends Fragment {
 
     private FragmentSignInBinding binding;
     private FirebaseAuth mAuth;
-    private NavController navController;
+    private NavController mNav;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return (binding = FragmentSignInBinding.inflate(inflater, container, false)).getRoot();
     }
 
@@ -37,25 +33,21 @@ public class SignInFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        navController = Navigation.findNavController(view);
+        mAuth = FirebaseAuth.getInstance();
+        mNav = Navigation.findNavController(view);
 
         binding.emailSignIn.setOnClickListener(v -> {
             String email = binding.email.getText().toString();
             String password = binding.password.getText().toString();
 
-            // registrar
-
-            mAuth = FirebaseAuth.getInstance();
             mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    navController.navigate(R.id.action_signInFragment_to_chatFragment);
-                } else {
-                    Toast.makeText(requireContext(), "Error sign in", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            // loguear
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            mNav.navigate(R.id.action_signInFragment_to_chatFragment);
+                        } else {
+                            Log.e("ABCD", task.getResult().toString());
+                        }
+                    });
         });
     }
 }
